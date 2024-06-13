b223a53e4fbee62bcc5c62b2508ed80097ca3b7a
Ernesto Domato <edomato@gmail.com>
2024-06-13 17:39:28 -0300

    Add Strategy and State implementation for App
    * Strategy para los modos manual y automatico
    * State para el estado de movimiento
    * agregado el diagrama en formato PNG
    
    Signed-off-by: Ernesto Domato <edomato@gmail.com>

---
+++ src/ar/edu/unq/po2/sem/Driving.java
@@ -0,0 +1,20 @@
+package ar.edu.unq.po2.sem;
+
+public class Driving implements EstadoMovimiento {
+    private App app;
+
+    public Driving(App app) {
+        this.app = app;
+    }
+
+    @Override
+    public void driving() {
+    }
+
+    @Override
+    public void walking() {
+        EstadoMovimiento nuevoEstado = new Walking(this.app);
+        this.app.setEstado(nuevoEstado);
+        this.app.iniciarEstacionamiento();
+    }
+}
